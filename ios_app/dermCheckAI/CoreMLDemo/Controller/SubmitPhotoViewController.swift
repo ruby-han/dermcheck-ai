253f330be926c83d5fbbf938ad7fda14c9416f6b
Ruby Han <rubyhan@berkeley.edu>
2022-11-29 11:09:38 -0600

    updating ios files

--- ios_app/dermCheckAI/CoreMLDemo/Controller/SubmitPhotoViewController.swift
+++ ios_app/dermCheckAI/CoreMLDemo/Controller/SubmitPhotoViewController.swift
@@ -15,7 +15,7 @@ class SubmitPhotoViewController: UIViewController, UINavigationControllerDelegat
     @IBOutlet weak var classifier: UILabel!
     
     // Declare ML Model
-    var model: resnet_10e_SGD_GS9!
+    var model: split_8_resnet_10e_AdamW!
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -26,7 +26,7 @@ class SubmitPhotoViewController: UIViewController, UINavigationControllerDelegat
     
     override func viewWillAppear(_ animated: Bool) {
         // Initialize ML model
-        model = resnet_10e_SGD_GS9()
+        model = split_8_resnet_10e_AdamW()
     }
     
     override func didReceiveMemoryWarning() {
